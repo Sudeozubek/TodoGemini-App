b85f3c2f850a1d3dec66dcb055538ed360af31cb
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-02-12 00:22:22 +0300

    migration dosyaları

---
+++ alembic/versions/0ccc81efafcf_phone_number_added.py
@@ -0,0 +1,26 @@
+"""phone number added
+
+Revision ID: 0ccc81efafcf
+Revises: 
+Create Date: 2025-02-09 16:24:18.625242
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0ccc81efafcf'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
